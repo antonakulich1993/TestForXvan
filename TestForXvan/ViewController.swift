f644edc98f82a8a6dc2979ee1fd62693591c38bd
Anton <antonakulich1993@gmail.com>
2021-12-31 00:42:47 +0300

    initial commit

---
+++ TestForXvan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestForXvan
+//
+//  Created by MacBook on 31.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
